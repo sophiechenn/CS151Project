6cb3e357322b940b40fcb490bb71ffc5dba8e80b
sophiechenn <39249028+sophiechenn@users.noreply.github.com>
2018-12-02 15:14:47 -0800

    Update HomeScreen.java

--- HomeScreen.java
+++ HomeScreen.java
@@ -1,45 +1,52 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class HomeScreen {
-
-	public static void main (String[] args)
+	
+	public HomeScreen()
 	{
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Hotel Reservation");
-		
-		//JPanel panel = new JPanel(new BorderLayout());
-		//panel.setBackground(new Color(136, 189, 188));
-		
-		HomeScreenDesign design = new HomeScreenDesign();
-		//text.setForeground(Color.WHITE);
-		design.setBackground(new Color(136, 189, 188));
-		
-		/*JLabel label = new JLabel("HOTEL RESERVATION SYSTEM", JLabel.CENTER);
-		label.setForeground(Color.WHITE);
-		label.setFont(new Font("Impact", Font.PLAIN, 40));
-		
-		JLabel space = new JLabel();*/
+		frame.setVisible(true);
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setBackground(new Color(136, 189, 188));
+		
 		JButton mButton = new JButton("Manager");
+		mButton.addActionListener(new
+				ActionListener()
+				{
+					public void actionPerformed (ActionEvent e)
+					{
+						frame.setVisible(false);
+						new ManagerHome();
+						
+					}
+				});
+		
 		JButton gButton = new JButton("Guest");
+		gButton.addActionListener(new
+				ActionListener()
+				{
+					public void actionPerformed (ActionEvent e)
+					{
+						frame.setVisible(false);
+					}
+				});
+		
 		buttonPanel.add(mButton);
 		buttonPanel.add(gButton);
 		
+		HomeScreenDesign design = new HomeScreenDesign();
+		design.setBackground(new Color(136, 189, 188));
 		design.add(buttonPanel, BorderLayout.SOUTH);
-		//panel.add(space, BorderLayout.NORTH);
-		//panel.add(label, BorderLayout.CENTER);
-		
-		//panel.add(text);
-		///panel.add(buttonPanel);
-		
-		//frame.add(panel);
+
 		frame.add(design);
 		frame.setSize(500, 500);
-		frame.setVisible(true);
 	}
 	
 }
