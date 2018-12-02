3a611ac42b77290031cced30a93bcd4a4f873b76
sophiechenn <39249028+sophiechenn@users.noreply.github.com>
2018-12-02 15:15:16 -0800

    Update ManagerHome.java

--- ManagerHome.java
+++ ManagerHome.java
@@ -1,25 +1,68 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class ManagerHome {
 
-	public static void main (String[] args)
-	{
+	private Manager m;
+	
+	public ManagerHome()
+	{		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
-		buttonPanel.setBackground(new Color(36, 48, 94));
-		JButton button1 = new JButton("Load");
+		
 		Color blue = new Color(168, 208, 230);
+		
+		buttonPanel.setBackground(new Color(36, 48, 94));
+		
+		JButton button1 = new JButton("Load");	
 		button1.setBackground(blue);
+		button1.addActionListener(new
+				ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						m.load();
+					}
+				});
+		
 		JButton button2 = new JButton("View");
 		button2.setBackground(blue);
+		button2.addActionListener(new
+				ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						m.view();
+					}
+				});
+		
 		JButton button3 = new JButton("Save");
 		button3.setBackground(blue);
+		button3.addActionListener(new
+				ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						m.save();
+					}
+				});
+		
 		JButton button4 = new JButton("Quit");
 		button4.setBackground(blue);
+		button4.addActionListener(new
+				ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						m.quit();
+					}
+				});
 		
 		buttonPanel.add(button1);
 		buttonPanel.add(button2);
