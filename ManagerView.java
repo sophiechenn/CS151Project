21cef537bf4f59ae56fdf715d4511edcc66f69cc
sophiechenn <39249028+sophiechenn@users.noreply.github.com>
2018-12-06 13:59:58 -0800

    Update ManagerView.java

--- ManagerView.java
+++ ManagerView.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.GregorianCalendar;
 
@@ -11,25 +12,35 @@ public class ManagerView {
 
 	private static CalendarView cal;
 	private static GregorianCalendar g;
+	private Manager m;
 	
 	public static void main(String[] args)
 	{
 		g = new GregorianCalendar();
-		JFrame frame = new JFrame();
 		cal =  new CalendarView(g);
-		JPanel panel = cal.getMonth();
+		JFrame frame = new JFrame();
+		JPanel control1 = cal.getMonth();
 		
 		cal.addChangeListener(new ChangeListener(){
 
 			public void stateChanged(ChangeEvent e) {
-				//Change available rooms
-				//display them
+				//Change available rooms 
 				
 			}
 			
 		});
+		
+		JPanel view1 = new JPanel();
+		view1.setBackground(Color.BLUE);
+		JPanel control2 = new JPanel();
+		control2.setBackground(Color.BLACK);
+		JPanel view2 = new JPanel();
+		view2.setBackground(Color.RED);
 		frame.setLayout(new GridLayout(2,2));
-		frame.add(panel);
+		frame.add(control1);
+		frame.add(view1);
+		frame.add(control2);
+		frame.add(view2);
 		frame.setVisible(true);
 		frame.setSize(1000, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
